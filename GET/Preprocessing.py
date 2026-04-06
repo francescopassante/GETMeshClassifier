27240aeb1258bd37798d96fb27ef7d97a1e518b1
francescopassante <francescopassante@icloud.com>
2026-04-07 00:06:23 +0200

    Implemented subsampling to speed up computations

--- GET/Preprocessing.py
+++ GET/Preprocessing.py
@@ -1,37 +1,40 @@
 import numpy as np
 import potpourri3d as pp3d
+import torch
 import trimesh
 
 
 class MeshPreprocessor:
-    def __init__(self, path):
-        self.mesh = self.preprocess_mesh(path)
+    def __init__(self, path, subsample=0.1):
+        self.mesh = self.preprocess_mesh(path, subsample)
 
-    def preprocess_mesh(self, mesh_path):
+    def preprocess_mesh(self, mesh_path, subsample):
         # Load the mesh
         mesh = trimesh.load(mesh_path)
-        # we compute its surface area by summing up the areas of all faces, and then scale it into 1
-        area = mesh.area
-        if area > 0:
-            mesh.apply_scale(1 / area)
-        return mesh
 
-    def compute_geodesic_neighborhood(self):
+        simplified_mesh = mesh.simplify_quadric_decimation(percent=1 - subsample)
+
+        # we compute its surface area by summing up the areas of all faces, and then scale it into 1
+        area = simplified_mesh.area
+        if area > 0:
+            simplified_mesh.apply_scale(1 / np.sqrt(area))
+
+        return simplified_mesh
+
+    def compute_geodesic_neighborhood(self, p_idx, distance):
 
         solver = pp3d.MeshHeatMethodDistanceSolver(self.mesh.vertices, self.mesh.faces)
 
         # 3. Compute distances from a source vertex p (index p_idx)
-        p_idx = 5000
         distances = solver.compute_distance(p_idx)
 
         # 4. Select vertices within the 0.2 geodesic radius
-        neighbor_indices = np.where(distances <= 0.1)[0]
+        neighbor_indices = np.where(distances <= distance)[0]
         return neighbor_indices
 
     # Function to plot the neighbors of vertex 0:
-    def plot_neighbors(self, neighbor_indices):
+    def plot_neighbors(self, p_idx, neighbor_indices):
         import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
@@ -52,9 +55,9 @@ class MeshPreprocessor:
 
         # Plot the source vertex in blue
         ax.scatter(
-            self.mesh.vertices[5000, 0],
-            self.mesh.vertices[5000, 1],
-            self.mesh.vertices[5000, 2],
+            self.mesh.vertices[p_idx, 0],
+            self.mesh.vertices[p_idx, 1],
+            self.mesh.vertices[p_idx, 2],
             color="blue",
             s=50,
         )
@@ -65,7 +68,11 @@ class MeshPreprocessor:
 if __name__ == "__main__":
     # Example usage
     path = "data/SHREC11_test_database_new/T42.off"  # Replace with your mesh file path
-    preprocessor = MeshPreprocessor(path)
-    neighbours = preprocessor.compute_geodesic_neighborhood()
-    # Use function to plot the neighbors of vertex 0:
-    preprocessor.plot_neighbors(neighbours)
+    preprocessor = MeshPreprocessor(path, subsample=0.1)
+    print("total vertices: ", len(preprocessor.mesh.vertices))
+    neighbor_indices = preprocessor.compute_geodesic_neighborhood(
+        p_idx=100, distance=0.2
+    )
+    print("neighbors: ", len(neighbor_indices))
+    preprocessor.plot_neighbors(100, neighbor_indices)
+    preprocessor.mesh.show()
